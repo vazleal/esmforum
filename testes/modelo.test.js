428631f188e28b746109da5b4403e4a73217d5bb
vazleal <vazleal@outlook.com>
2023-05-30 23:37:32 -0300

    feat: 100% code coverage

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -17,9 +17,26 @@ test('Testando cadastro de três perguntas', () => {
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
   const perguntas = modelo.listar_perguntas(); 
+  const pergunta_0 = modelo.get_pergunta(perguntas[0].id_pergunta);
+  const pergunta_1 = modelo.get_pergunta(perguntas[1].id_pergunta);
   expect(perguntas.length).toBe(3);
-  expect(perguntas[0].texto).toBe('1 + 1 = ?');
-  expect(perguntas[1].texto).toBe('2 + 2 = ?');
-  expect(perguntas[2].num_respostas).toBe(0);
+  expect(pergunta_0.texto).toBe('1 + 1 = ?');
+  expect(pergunta_1.texto).toBe('2 + 2 = ?');
+  expect(perguntas[1].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+});
+
+test('Testando cadastro de três respostas', () => {
+  modelo.cadastrar_pergunta('1 + 1 = ?');
+  modelo.cadastrar_pergunta('2 + 2 = ?');
+  modelo.cadastrar_pergunta('3 + 3 = ?');
+  modelo.cadastrar_resposta(0, '2');
+  modelo.cadastrar_resposta(1, '4');
+  modelo.cadastrar_resposta(2, '6');
+  const respostas_pergunta_0 = modelo.get_respostas(0);
+  const respostas_pergunta_1 = modelo.get_respostas(1);
+  expect(respostas_pergunta_0.length).toBe(1);
+  expect(respostas_pergunta_0[0].texto).toBe('2');
+  expect(respostas_pergunta_1[0].texto).toBe('4');
+  expect(modelo.get_num_respostas(1)).toBe(1);
 });
